d106c110668498366b458fe04bf914141af2c12a
ssteuteville <ssteuteville@gmail.com>
2013-10-29 19:55:26 -0700

    added redirect on home#home action for logged in user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
   	if params[:search] != "" and !params[:search].nil?
   		redirect_to(doi_path(params[:search]))
   	end
+  	if user_signed_in?
+  		redirect_to dois_path
+  	end
   end
 end
